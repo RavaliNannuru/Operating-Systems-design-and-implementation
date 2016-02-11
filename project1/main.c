d7076295e9b02f50caafab26673429844d4faad8
balajisuiaji474 <balajisu.iaji474@gmail.com>
2016-02-11 00:42:31 -0600

    new file

--- project1/main.c
+++ project1/main.c
@@ -11,6 +11,8 @@
 pid_t pid1;
 
 int execCommands(char **);
+int exec_io_redirection(char **);
+void SIGCHLD_handler(int);
 
 char **splitInputCommands(char *my_string)
 {
@@ -47,8 +49,79 @@ void chld_SIGQUIT_handler(int num)
 {
 	exit(0);
 }
+int exec_io_redirection(char **args) {
 
-int parseArguments(char **args)
+	int command_count = 0;
+	int output_loc = -1;
+	int status = 0;
+	pid_t wpid;
+	char **res;
+	char *choice;
+
+	for (char **temparg = args; *temparg; ++temparg) {
+		if(strstr(*temparg, "=>") != NULL) {
+			output_loc = command_count;
+		}
+		command_count++;
+	}
+
+	if (command_count >= 3) {
+		pid_t pidp = getpid(),pid2;
+		pid1 = fork();
+		if (pid1 == -1) {
+			perror("Failed during fork");
+			status = 1;
+		} else if (pid1 == 0) {
+
+
+			res = malloc((output_loc) * sizeof(char*));
+			for(int i = 0; i < output_loc;i++) {
+				res[i] = args[i];
+			}
+			char *output = args[output_loc + 1];
+
+			int fd1 = creat(output , 0644) ;
+			dup2(fd1, STDOUT_FILENO);
+			close(fd1);
+
+			if (execvp(res[0], res)==-1){
+				perror("Failed during execution of commands");
+			}
+			exit(0);
+
+		} else {
+			pid2 = fork();
+			if (pid2 == 0) {
+				if (signal(SIGQUIT, chld_SIGQUIT_handler) == SIG_ERR) {
+					printf("SIGINT install error\n");
+					exit(1);
+				}
+				while(1){
+					sleep(5);
+					printf("please enter [y]es, [n]o to terminate the program:");
+					scanf("%c", &choice);
+					//printf("out:%c",choice);
+					if (choice == 'y') {
+						puts("inside");
+						kill(pidp, SIGCHLD);
+						exit(0);
+					}
+				}
+			}
+			waitpid(pid1,0,0);
+		}
+
+		kill(pid2, SIGQUIT);
+		waitpid(pid2,0,0);
+		free(res);
+		return 1;
+	} else {
+		printf("Error in number of inputs");
+	}
+	return status;
+}
+
+int parseArguments(char **args, int is_io)
 {
 
 	int status=0;
@@ -57,6 +130,9 @@ int parseArguments(char **args)
 	{
 		puts("shell is gone exit");
 		exit(0);
+	}
+	else if (is_io == 1) {
+		return exec_io_redirection(args);
 	}
 	else
 	{
@@ -75,6 +151,10 @@ int execCommands(char **args)
 	char choice;
 	pid_t pidp, pid2, wpid;
 	pidp = getpid();
+	if (signal(SIGCHLD, SIGCHLD_handler) == SIG_ERR) {
+		printf("SIGINT install error\n");
+		exit(1);
+	}
 	pid1 = fork();
 	if (pid1 == 0) {
 		if (execvp(args[0], args) == -1) {
@@ -95,9 +175,10 @@ int execCommands(char **args)
 			while(1){
 				sleep(5);
 				printf("please enter [y]es, [n]o to terminate the program:");
-				scanf("%c", &choice);
-				fflush(stdin);
-				printf("out:%c",choice);
+				choice=getchar();
+				printf("yo",choice);
+
+				//putchar(choice);
 				if (choice == 'y') {
 					puts("inside");
 					kill(pidp, SIGCHLD);
@@ -105,18 +186,11 @@ int execCommands(char **args)
 				}
 			}
 		}
-		do{
-			wpid = waitpid(pid1, &status, WUNTRACED);
-
-		}while(!WIFEXITED(status) && !WIFSIGNALED(status));
+		waitpid(pid1, 0,0);
 
 	}
 	kill(pid2, SIGQUIT);
-	do{
-		wpid = waitpid(pid2, &status, WUNTRACED);
-
-	}while(!WIFEXITED(status) && !WIFSIGNALED(status));
-
+	waitpid(pid2, 0, 0);
 	return 1;
 
 }
@@ -132,6 +206,7 @@ int main( void ) {
 	char *my_string;
 	// after parsing of commands, o/p is stored in args
 	char **arguments;
+	int contains_io =0;
 
 	if (signal(SIGCHLD, SIGCHLD_handler) == SIG_ERR) {
 		printf("SIGINT install error\n");
@@ -154,8 +229,14 @@ int main( void ) {
 			puts ("Entered Commands...");
 			puts (my_string);
 		}
+		contains_io=0;
+		if (strstr(my_string, "=>") != NULL) {
+			contains_io=1;
+		}
+
 		arguments = splitInputCommands(my_string);
-		parseArguments(arguments);
+
+		parseArguments(arguments,contains_io);
 	}
 	//free the allocated memory
 	free(my_string);
